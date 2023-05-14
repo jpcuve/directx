7f327fd7ca8fb406ec9feefd574c030afa0ee79d
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2023-05-14 22:32:42 +0200

    .

--- src/directx.cpp
+++ src/directx.cpp
@@ -10,13 +10,13 @@ DebugStream dbg;
 std::unique_ptr<Application> app;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow) {
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
     dbg << "Starting directx demo" << std::endl;
-    for (int i = -10; i < 10; i++){
+    for (int i = -10; i < 10; i++) {
         dbg << i << " " << PositiveModulo(i, 3) << std::endl;
     }
     dbg << std::flush;
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    if (message == WM_DESTROY){
+    if (message == WM_DESTROY) {
         PostQuitMessage(0);
         return 0;
     }
